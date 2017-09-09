f720227a1ae8070d156033e21495f902f11fa872
George Spake <gpspake@gmail.com>
2017-09-09 14:33:22 -0500

    connect to db and render data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import { database } from './firebase'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    database.ref().on('value', (snapshot) => {
+      this.setState({
+        data: snapshot.val()
+      }
+    )
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,7 +26,7 @@ class App extends Component {
           <h2>Welcome to React and Firebase</h2>
         </div>
         <pre className="App--data">
-          One day, some data from Firebase will go here.
+          { JSON.stringify(this.state.data, null, 2) }
         </pre>
       </div>
     );
